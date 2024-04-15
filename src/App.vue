42a2945946ecbd484fa105715fb2fc85eb6711a4
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-15 15:53:12 +0200

    chore: add basic structure for card "deck"

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,39 @@
 <script>
+	import AppDeck from "./components/AppDeck.vue"
+	import { store } from "./store.js";
+	import axios from 'axios';
 
+	export default{
+		components: {
+			AppDeck,
+		},
+		data(){
+			return {
+				queryParams: {
+					num: 20,
+					offset: 0
+				}
+			}
+		},
+		methods: {
+			getCardsFromApi(){
+				axios.get("https://db.ygoprodeck.com/api/v7/cardinfo.php", { params: this.queryParams })
+				.then((response) => {
+					store.deck = response.data.data;
+					console.log(store.deck)
+				})
+			}
+		},
+		mounted(){
+			//this.getCardsFromApi()
+		}
+	}
 </script>
 
 <template>
-
+	<AppDeck></AppDeck>
 </template>
 
 <style lang="scss">
-	@use './style/partials/variables' as *;
+	@use './style/general' as *;
 </style>
---
+++ src/components/AppDeck.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+        name: 'AppDeck'
+    }
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+</template>
+
+<style scoped lang="scss">
+    main{
+        background-color: salmon;
+        padding: 40px 0px;
+    }
+
+    .container{
+        max-width: 1100px;
+        background-color: white;
+        margin: 0 auto;
+    }
+</style>
